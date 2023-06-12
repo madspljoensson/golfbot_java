78d197bfd9f7c8a341ac469f8f65e08e79229b6e
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2023-06-12 16:38:25 +0200

    collectCornerBall method and wait method

--- src/golfbot1/RobotTest.java
+++ src/golfbot1/RobotTest.java
@@ -1,6 +1,7 @@
 package golfbot1;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class RobotTest {
 
@@ -8,12 +9,13 @@ public class RobotTest {
 		
 		EV3Skeleton robot = new EV3Skeleton();
 		
-		while(true) {
-			//robot.forwardWheelEntrance();
-			robot.backwardWheelEntrance();
-			//robot.shoot();
-		}
-		
+        robot.forwardWheelEntrance();
+        
+        robot.collectCornerBall();
+
+        robot.forwardWheelEntrance();
+
+		robot.wait(5000);
 		
 		//robot.shoot();
 		
